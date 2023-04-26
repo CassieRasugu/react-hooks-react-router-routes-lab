d48442b7083a98dfdbf307e5f7b6a5ddc5eee31b
CassieRasugu <cassierasugu@gmail.com>
2023-04-26 09:28:07 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+   return (
+      <div>
+        <NavLink exact to="/movies" activeClassName="active">Movies</NavLink>
+        <NavLink exact to="/directors" activeClassName="active">Directors</NavLink>
+        <NavLink exact to="/actors" activeClassName="active">Actors</NavLink>
+        <NavLink exact to="/" activeClassName="active">Home</NavLink>
+  </div>
+  );
 }
-
 export default NavBar;
