7fb00c24165dcb52e797729c5492121a430e0667
CassieRasugu <cassierasugu@gmail.com>
2023-04-26 09:16:50 +0300

    Update Actors.js

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,14 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+     <nav>
+      <ul>
+        <directors to="/actors" activeClassName="active"><h1>Actors Page</h1> </directors>
+      </ul>
+    </nav>
+</div>
+    );
 }
-
 export default Actors;
